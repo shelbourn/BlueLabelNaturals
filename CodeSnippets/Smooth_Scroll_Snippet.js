9f494cddc5fb60f6a49515a2d2c341454478f332
shelbourn <mattshelbourn@gmail.com>
2019-08-19 12:57:53 -0700

    Renaming folders in file tree.

---
+++ CodeSnippets/Smooth_Scroll_Snippet.js
@@ -0,0 +1,29 @@
+jQuery( function($){
+    var win = $( window );
+    
+    function bbScroll(){
+        if ( 'undefined' != typeof FLBuilderLayoutConfig ) {    
+            var offset = 70;
+            
+            if ( 'undefined' === typeof FLBuilderLayout ) {
+                return;
+            }
+            
+            if ( FLBuilderLayout._isMobile() && win.width() < 992 ) {
+                offset = 0;
+            }
+            
+            if ( $( 'body.admin-bar' ).length > 0 ) {
+                offset += 32;
+            }
+            
+            FLBuilderLayoutConfig.anchorLinkAnimations.duration = 1000;
+            FLBuilderLayoutConfig.anchorLinkAnimations.easing = 'swing';
+            FLBuilderLayoutConfig.anchorLinkAnimations.offset = offset;
+        }
+    }
+    
+    bbScroll();
+    win.on( 'resize', bbScroll );
+    
+} );
